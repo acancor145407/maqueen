2352840677c64fc94ca8d873a508e7220a668c44
acancor145407 <98743495+acancor145407@users.noreply.github.com>
2022-03-09 03:28:37 -0500

    gh

--- main.ts
+++ main.ts
@@ -1,16 +1,28 @@
+function girar90dcha () {
+    maqueen.motorStop(maqueen.Motors.All)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 150)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 150)
+    basic.pause(350)
+}
+function girar90izq () {
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 150)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 150)
+    basic.pause(350)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 150)
+    basic.pause(500)
+    maqueen.motorStop(maqueen.Motors.All)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 150)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 150)
+    basic.pause(350)
+}
 basic.forever(function () {
-    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 10) {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 5) {
         maqueen.motorStop(maqueen.Motors.All)
         basic.pause(50)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 150)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 150)
-        basic.pause(350)
+        girar90izq()
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 150)
-        basic.pause(500)
-        maqueen.motorStop(maqueen.Motors.All)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 150)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 150)
-        basic.pause(350)
+        basic.pause(800)
+        girar90dcha()
     } else {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 150)
         basic.pause(50)
